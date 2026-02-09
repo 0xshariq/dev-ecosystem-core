0c41646512283cab14cdc5ea19aac9cc86f5c799
Sharique Chaudhary <khanshariq92213@gmail.com>
2026-02-09 18:12:20 +0000

    feat: add global types export for unified type management in the ecosystem

---
+++ src/types/global.types.ts
@@ -0,0 +1,16 @@
+/**
+ * Global Types
+ * 
+ * Central export point for all core types in the dev-ecosystem.
+ * This provides a unified import location for commonly used types.
+ * 
+ * @module types/global
+ */
+
+// Adapter types
+export * from './adapter.types.js';
+
+// Workflow types
+export * from './workflow.interfaces.js';
+export * from './workflow.enums.js';
+export * from './workflow.type.js';
